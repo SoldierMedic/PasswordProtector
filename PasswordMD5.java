b6f8022fe94ce0edc3102a3e006928383128bb2a
yoitspattyo <43910669+yoitspattyo@users.noreply.github.com>
2019-05-01 11:37:11 -0600

    5/1 after class
    first and second options work. MD5Passwords just created as a global array variable

--- PasswordMD5.java
+++ PasswordMD5.java
@@ -5,12 +5,14 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /*
- * Author:	Patrick and Marco
+ * Author:		Patrick and Marco
  * Date:		5/13/2019
- * Class:	CSC 160 Combo
+ * Class:		CSC 160 Combo
  * Assignment:	Final Project
  * 
- * This program ......
+ * This object is used to convert plaintext strings into their MD5 hashes. The methods allow you to get or set the plaintext, or get the MD5 hash. You cannot set the MD5 hash, as this is an object
+ * meant to generate MD5 hashes since they are not reversible.
+ * 
  */
 
 public class PasswordMD5
@@ -23,6 +25,7 @@ public class PasswordMD5
 		plaintext = plaintext1;
 	}
 	
+	// Author
 	// This method will determine the MD5 hash of a plaintext string, returning the value of the hash
 public String getMD5Hash()
 {
@@ -41,12 +44,14 @@ public String getMD5Hash()
 	return hashInteger.toString(16);	
 }
 	
-		//	This method will print the plaintext of this password/hash pair
+		// Author
+		// This method will print the plaintext of this password/hash pair
 	public String getPlaintext()
 	{
 		return plaintext;
 	}
 	
+		// Author
 		// This method will allow a user to set the plaintext for a pair. If plaintext is set, new hash is determined
 	public void setPlaintext(String plaintext1)
 	{
--- PasswordProtector.java
+++ PasswordProtector.java
@@ -5,31 +5,37 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /*
- * Author:	Patrick and Marco
+ * Author:		Patrick and Marco
  * Date:		5/13/2019
- * Class:	CSC 160 Combo
+ * Class:		CSC 160 Combo
  * Assignment:	Final Project
  * 
- * This program ......
+ * This program is the driver class for the Final Project. Main is a Main Menu that throws the user's choice to one of the methods. Before the user can reach the Main Menu, there is a login process (WORK IN PROGRESS)
+ * The user can convert a list of plaintext strings to MD5 hashes for user in determining a list of unknown hashes (WORK IN PROGRESS). Users can also check if their password appears in any passwords used in lists
+ * in the program, thus telling the user they have a weak password (WORK IN PROGRESS). Finally, users can interact with the password database that allowed them to login, by adding new users or displaying the database
+ * file to display differences between salting and not salting passwords (WORK IN PROGRESS)
+ * 
  */
 
 public class PasswordProtector
 {
 	public static Scanner input = new Scanner(System.in);
+	public static String[][] mD5PasswordArray = new String[10000][2];	// array made from the password file; plaintext in column 0, md5Hash column 1
 
 	public static void main( String[ ] args )
 	{
-		String[][] passwordArray = null;		// array made from the password file; plaintext in column 0, md5Hash column 1
-		int userChoice;							// option user chooses for what they would like to do
+		int userChoice;											// option user chooses for what they would like to do
+		
+		userLogin();
 		
 		do
 		{
 			System.out.printf("\nWhat would you like to do?\n\n");
 			System.out.printf("1) Convert file of plaintext strings to MD5 hashes\n");
 			System.out.printf("2) Determine plaintext of list of unknown hashes\n");
-			System.out.printf("3) \n");
-			System.out.printf("4) \n");
-			System.out.printf("5) \n");
+			System.out.printf("3) Check if your password is weak\n");
+			System.out.printf("4) Add new user\n");
+			System.out.printf("5) Show password database\n");
 			
 			userChoice = input.nextInt();
 			while (userChoice < 1 && userChoice > 5)
@@ -40,34 +46,47 @@ public class PasswordProtector
 			
 			if (userChoice == 1)
 			{
-				passwordArray = convertToMD5Menu();
+				convertToMD5Menu();
 			}
 			else if (userChoice == 2)
 			{
-				determinePlaintextMenu(passwordArray);
+				determinePlaintextMenu(mD5PasswordArray);
 			}
 			else if (userChoice == 3)
 			{
-				
+				checkWeakPassword();
 			}
 			else if (userChoice == 4)
 			{
-				
+				addNewUser();
 			}
 			else
 			{
-				
+				showPasswordDatabase();
 			}
 		} while (userChoice >= 1 && userChoice <= 5);
 	}
 	
-		// Author:	
-		//	This method
-	public static String[][] convertToMD5Menu()
+		// Author
+		// This method
+	public static void addNewUser()
 	{
-		int userChoice;							// option user chooses for what they would like to do
-		String fileChoice;						// file user specifies they would like to use for MD5 conversion
-		String[][] passwordArray = null;		// array made from the password file; plaintext in column 0, md5Hash column 1
+		
+	}
+	
+		// Author
+		// This method
+	public static void checkWeakPassword()
+	{
+		
+	}
+	
+		// Author:	
+		// This method
+	public static void convertToMD5Menu()
+	{
+		int userChoice;										// option user chooses for what they would like to do
+		String fileChoice;									// file user specifies they would like to use for MD5 conversion
 		
 		System.out.printf("\nWhat file would you like to convert?\n\n");
 		System.out.printf("1) Convert default file\n");
@@ -83,30 +102,39 @@ public class PasswordProtector
 		if (userChoice == 1)
 		{
 			fileChoice = "Plaintext.txt";
-			passwordArray = readFileToPasswordArray(fileChoice);
+			readFileToPasswordArray(fileChoice);
+			
+			for (int i = 0; i < mD5PasswordArray.length; i++)
+			{
+				System.out.printf("%10s%40s\n", mD5PasswordArray[i][0], mD5PasswordArray[i][1]);
+			}
 		}
 		else
 		{
 			System.out.printf("What file would you like to open? Specify its path\n");
 			fileChoice = input.next();
 			
-			passwordArray = readFileToPasswordArray(fileChoice);
+			readFileToPasswordArray(fileChoice);
 		}
 		
 		System.out.printf("Your file has been read to an array with hashes\n");
-		return passwordArray;
 	}
 	
 		// Author:
 		// This method
 	public static void determinePlaintext(String[][] knownPasswords, String file1)
 	{
-		int counter = 0;								// holds how many hashes have been read to the array
-		Scanner unknownHashes = null;				// scanner object reading in the file of unknown hashes
-		String determinedPlaintext;				// holds value of string of determined plaintext of a hash
-		String testHash;								// holds value of known hash from knownPasswords array
-		String unknownHash;							// holds value of string of unknown hash
+		int counter = 0;									// holds how many hashes have been read to the array
+		Scanner unknownHashes = null;						// scanner object reading in the file of unknown hashes
+		String determinedPlaintext;							// holds value of string of determined plaintext of a hash
+		String testHash;									// holds value of known hash from knownPasswords array
+		String unknownHash;									// holds value of string of unknown hash
 		String[][] unknownPasswords = new String[10][2];	// holds unknown hash in column 0, determined (if possible) string value in column 1
+		
+		for (int i = 0; i < knownPasswords.length; i++)
+		{
+			System.out.printf("%10s%40s\n", knownPasswords[i][0], knownPasswords[i][1]);
+		}
 		
 		try
 		{
@@ -118,7 +146,7 @@ public class PasswordProtector
 				for (int i = 0; i < knownPasswords.length; i++)
 				{
 					testHash = knownPasswords[i][1];
-					if (unknownHash.equals(testHash))			// Check unknown hash against known hashes
+					if (unknownHash.equals(testHash))		// Check unknown hash against known hashes
 					{
 						determinedPlaintext = knownPasswords[i][0];
 						unknownPasswords[counter][1] = determinedPlaintext;
@@ -139,26 +167,24 @@ public class PasswordProtector
 				}
 			}
 		}
-		catch ( FileNotFoundException e )
+		catch (FileNotFoundException e)
 		{
-			System.out.println ( "catching" );
 			e.printStackTrace();
 		}
 		finally
 		{
 			if (unknownHashes != null)
 			{
-				System.out.println ( "ending" );
 				unknownHashes.close();
 			}
 		}
 	}
 	
 		// Author:	
-		//	This method
+		// This method creates a menu that allows a user to choose different unknown hash files to try and determine their identities
 	public static void determinePlaintextMenu(String[][] knownPasswords)
 	{
-		int userChoice;							// option user chooses they would like to do
+		int userChoice;										// option user chooses they would like to do
 		
 		System.out.printf("\nWhat file would you like the determine the plaintext of the hashes?\n\n");
 		System.out.printf("1) Small, MD5 only file\n");
@@ -177,37 +203,36 @@ public class PasswordProtector
 		}
 		else if (userChoice == 2)
 		{
-			
+			determinePlaintext(knownPasswords, "LongMD5.txt");
 		}
 		else
 		{
-			
+			determinePlaintext(knownPasswords, "MixedHashes.txt");
 		}
 	}
 	
 		// Author:	
-		//	This method will read a file of plaintext strings to an array, then in the second column of the array store the MD5 hash of the string in the first column
-	public static String[][] readFileToPasswordArray(String plaintextFile)
+		// This method will read a file of plaintext strings to an array, then in the second column of the array store the MD5 hash of the string in the first column
+	public static void readFileToPasswordArray(String plaintextFile)
 	{
-		int passwordCounter = 0;					// Counts how many passwords have been read, to correctly assign them into the array
-		PasswordMD5 convertedPassword = null;	// Object that will convert plaintext to md5Hash
-		String[][] passwordArray = null;			// Array that holds plaintext in column 0, md5Hash in column 1
-		Scanner readPasswordFile = null;			// Scanner object reading in password file
-		String password;								//	Password from plaintext file
+		int passwordCounter = 0;							// Counts how many passwords have been read, to correctly assign them into the array
+		PasswordMD5 convertedPassword = null;				// Object that will convert plaintext to md5Hash
+		Scanner readPasswordFile = null;					// Scanner object reading in password file
+		String password;									// Password from plaintext file
 		
 		try
 		{
 			readPasswordFile = new Scanner(new File(plaintextFile));
-			passwordArray = new String[10000][2];
+			mD5PasswordArray = new String[10000][2];
 			
 			while (readPasswordFile.hasNextLine())
 			{
 				password = readPasswordFile.next();
-				passwordArray[passwordCounter][0] = password;
+				mD5PasswordArray[passwordCounter][0] = password;
 				convertedPassword = new PasswordMD5(password);
-				passwordArray[passwordCounter][1] = convertedPassword.getMD5Hash();
+				mD5PasswordArray[passwordCounter][1] = convertedPassword.getMD5Hash();
 				
-				System.out.printf("%10s%40s\n", passwordArray[passwordCounter][0], passwordArray[passwordCounter][1]);
+				System.out.printf("%10s%40s\n", mD5PasswordArray[passwordCounter][0], mD5PasswordArray[passwordCounter][1]);
 				
 				passwordCounter++;
 			}
@@ -223,14 +248,26 @@ public class PasswordProtector
 				readPasswordFile.close();
 			}
 		}
+	}
+	
+		// Author
+		// This method
+	public static void showPasswordDatabase()
+	{
 		
-		return passwordArray;
+	}
+	
+		// Author
+		// This method
+	public static void userLogin()
+	{
+		
 	}
 	
 	/*
 	 * 	Problems
 	 * 
-	 * 1)	asdf
+	 * 1)	Just the normal debugging issues as coding. Used wrong variable here, < instead of > there
 	 * 
 	 */
 
